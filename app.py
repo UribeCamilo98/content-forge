e995f4e73b9def6da826661d1c36f62abc0bd355
Camilo Andres Uribe Santos <camilouribe1998@gmail.com>
2026-05-18 08:55:04 -0500

    Implementacion de UI
    Se realizo una interfaz grafica para el procesamiento de los textos y la generacion de slides, ademas de eso se ajusto la estructura del proyecto.

--- app.py
+++ app.py
@@ -1,33 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from generator.carousel import Carrusel
 from utils.colors import obtener_paleta
 
-app = Flask (__name__)
+app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Hola Mundo"
+    return render_template("index.html")
 
-@app.route("/generar")
+@app.route("/generar", methods=["POST"])
 def generar():
-    texto = """Yo no quiero morirme sin saber de tu boca.
-Yo no quiero morirme con el alma perpleja
-sabiéndote distinto, perdido en otras playas.
-
-Yo no quiero morirme con este desconsuelo
-por el arco infinito de esa cúpula triste
-donde habitan tus sueños al sol de mediodía.
-
-Yo no quiero morirme sin haberte entregado
-las doradas esferas de mi cuerpo,
-la piel que me recubre, el temblor que me invade.
-
-Yo no quiero morirme sin que me hayas amado."""
-    paleta = obtener_paleta("vibrante")
-    plantilla = Carrusel(texto, paleta["fondo"], paleta["texto"], num_slides=20)
-    
+    texto = request.form["texto"]
+    nombre_paleta = request.form.get("paleta", "oscuro")
+    paleta = obtener_paleta(nombre_paleta)
+    plantilla = Carrusel(texto, paleta["fondo"], paleta["texto"])
     ruta = plantilla.generar()
 
-    return f"Carrusel generado en: {ruta}"
+    return render_template("index.html", ruta = ruta)
 
 if __name__ == "__main__":
     app.run(debug=True)
