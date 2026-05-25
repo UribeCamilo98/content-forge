32e715fd8452f364626952ca030d905aafb4b003
Camilo Andres Uribe Santos <camilouribe1998@gmail.com>
2026-05-25 12:29:25 -0500

    Test e Instrucciones
    Actualizacion en los tests de base.py y el archivo de instrucciones

--- tests/test_base.py
+++ tests/test_base.py
@@ -18,3 +18,18 @@ def test_texto_vacio_no_crashea():
     pb = PlantillaBase((0, 0, 0), (255, 255, 255))
     img = pb._crear_imagen("")
     assert isinstance(img, Image.Image)
+def test_contorno_agrega_pixeles():
+    pb = PlantillaBase((255, 255, 255), (0, 0, 0))
+    img_sin = pb._crear_imagen("Hola")
+    img_con = pb._crear_imagen("Hola", contorno_color=(255, 0, 0), contorno_grosor=3)
+    assert img_sin.tobytes() != img_con.tobytes()
+def test_sombra_agrega_pixeles():
+    pb = PlantillaBase((255, 255, 255), (0, 0, 0))
+    img_sin = pb._crear_imagen("Hola")
+    img_con = pb._crear_imagen("Hola", sombra_offset=(10, 10), sombra_color=(128, 128, 128))
+    assert img_sin.tobytes() != img_con.tobytes()
+def test_contorno_none_sin_cambio():
+    pb = PlantillaBase((255, 255, 255), (0, 0, 0))
+    img_sin = pb._crear_imagen("Hola")
+    img_con = pb._crear_imagen("Hola", contorno_color=None)
+    assert img_sin.tobytes() == img_con.tobytes()
